fab174061b1fc0e22f25cc7e9de8045d71e25967
ayyamrevanth <ayyamrevanth0708g@gmail.com>
2026-09-07 07:12:24 +0530

    Solving 0 Edge case

--- Week 1/countDigits.py
+++ Week 1/countDigits.py
@@ -1,7 +1,9 @@
 num=int(input("Enter num:"))
 count=0
-
-while num>0:
-    num= num // 10
-    count+=1
+if num==0:
+    count=1
+else:
+    while num>0:
+        num= num // 10
+        count+=1
 print(count)
